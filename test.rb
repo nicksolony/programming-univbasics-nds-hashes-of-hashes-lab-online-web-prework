d867e65a2f9bb9cd503fe791e3aab0a9f0d71fe6
Mykola Solonyy <nicksolony@gmail.com>
2019-11-01 01:10:27 +0000

    Automatically backed up by Learn

---
+++ test.rb
@@ -0,0 +1,19 @@
+mother_hash = {
+  name:"Alla Solona",
+  mother : {},
+  father: {}
+}
+
+father_hash = {
+  name:"Alex Solony",
+  mother: {},
+  father: {}
+}
+
+child = {
+  name: "Nick Solony",
+  mother: mother_hash,
+  father: father_hash
+}
+
+puts child
